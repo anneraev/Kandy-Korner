1dca9a73f805e3214131486aaf367fa1b1af7a67
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-21 20:42:20 -0500

    finished chapter 3 exercises

--- src/components/kandy-korner/KandyKorner.js
+++ src/components/kandy-korner/KandyKorner.js
@@ -1,45 +1,16 @@
 import React, { Component } from "react";
-import StoreList from "../stores/StoreList";
-import EmployeeList from "../employees/EmployeeList";
-import CandiesList from "../candies/CandiesList";
+import NavBar from "../navBar/NavBar";
+import ApplicationViews from "../ApplicationViews";
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default class KandyKorner extends Component {
-    storesFromAPI = [
-        {id: 1, name: "Nashvill North", address: "500 Wonka Avenue"},
-        {id: 2, name: "Nashville South", address: "666 Nero Place"}
-    ]
-
-    employeesFromAPI = [
-        {id: 1, name: "Bob"},
-        {id: 2, name: "Greg"},
-        {id: 3, name: "Julie"}
-    ]
-
-    candyTypesFromAPI = [
-        {id: 1, name: "chocolate"},
-        {id: 2, name: "hard"}
-    ]
-
-    candiesFromAPI = [
-        {id: 1, name: "chocolate bar", candyTypeId: 1},
-        {id: 2, name: "jawbreaker", candyTypeId: 2},
-        {id: 3, name: "rock candy", candyTypeId: 2}
-    ]
-    state = {
-        employees: this.employeesFromAPI,
-        stores: this.storesFromAPI,
-        candyTypes: this.candyTypesFromAPI,
-        candies: this.candiesFromAPI
-    }
-
     render() {
         return (
-            <article className ="kandy-korner">
-            <h1>Kandy Korner</h1>
-            <StoreList stores= {this.state.stores} />
-            <EmployeeList employees= {this.state.employees} />
-            <CandiesList candies= {this.state.candies} candyTypes={this.state.candyTypes} />
-            </article>
+            <React.Fragment>
+                <NavBar />
+                <ApplicationViews />
+            </React.Fragment>
         )
     }
 }
--- src/components/stores/StoreList.js
+++ src/components/stores/StoreList.js
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 export default class Stores extends Component {
     render() {
         return (
-            <section className="candies">
+            <section className="stores">
                 {
-                    this.props.stores.map(store => <div key={store.id}>
+                    this.props.stores.map(store =>
+                    <div key={store.id}>
                         <span>{store.name}</span>
                         <span>{store.address}</span>
                     </div>
